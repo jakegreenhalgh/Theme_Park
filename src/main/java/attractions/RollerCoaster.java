1c85aa52cec11ce824958535ed8101073b035d3a
Jacob <jacob_greenhalgh@hotmail.com>
2022-09-12 23:55:10 +0100

    Added interfaces

---
+++ src/main/java/attractions/RollerCoaster.java
@@ -0,0 +1,19 @@
+package attractions;
+
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class RollerCoaster  extends Attraction implements ISecurity {
+
+    public RollerCoaster(String name, int rating) {
+        super(name, rating);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() < 12 && visitor.getHeight() > 1.8) {
+            return true;
+        }
+        else return false;
+    }
+}
